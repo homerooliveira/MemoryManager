b05cf535681014ab495fd03724d1ee21fd8d0f4e
Homero Oliveira <homero.santos.001@acad.pucrs.br>
2018-11-04 23:20:39 -0200

    added defragment method and printFragmentation

--- MemoryManagerPlayground.playground/Contents.swift
+++ MemoryManagerPlayground.playground/Contents.swift
@@ -32,23 +32,44 @@ struct Block {
 
 extension Block: CustomStringConvertible {
     var description: String {
-        return "\(range) - isFree:\(isFree)"
+        let rangeDescription = "\(range.lowerBound)-\(range.upperBound)"
+        let isFreeDescription = isFree ? "livre" : "bloco \(id)"
+        let sizeDescription = "tamanho \(range.count)"
+        return "\(rangeDescription) \(isFreeDescription) (\(sizeDescription))"
     }
 }
 
 final class MemoryManager {
-    var blocks = [Block(id: -1, range: 100..<1250, isFree: true)]
+    var blocks: [Block] = []
+    
+    init(range: Range<Size>) {
+        let block = Block(id: -1, range: range, isFree: true)
+        blocks.append(block)
+    }
+    
+    func hasSpace(_ size: Size) -> Bool {
+        return blocks.firstIndex(where: { $0.hasSize(size) }) != nil
+    }
     
     func alloc(size: Int) {
         guard let index = blocks.firstIndex(where: { $0.hasSize(size) }) else {
+            printBlocks()
+            printFragmentation(size)
             return
         }
 
         let blocksFromSize = blocks[index].make(from: size)
         blocks.remove(at: index)
         blocks.insert(contentsOf: blocksFromSize, at: index)
+    }
+    
+    private func printFragmentation(_ size: Size) {
+        let freeSize = blocks.lazy
+            .filter { $0.isFree }
+            .map { $0.range.count }
+            .reduce(0, +)
         
-        printBlocks()
+        print("\(freeSize) livres, \(size) solicitados - fragmentação externa.")
     }
     
     func free(id: Int) {
@@ -57,18 +78,37 @@ final class MemoryManager {
         }
         blocks[index].id = -1
         blocks[index].isFree = true
-        printBlocks()
+        defragmentBlocks(index: index)
     }
     
     private func printBlocks() {
         blocks.forEach { (block) in
             print(block)
         }
-        print()
+    }
+    
+    private func defragmentBlocks(index: Int) {
+        var minIndex = index
+        var maxIndex = index
+        
+        if let minIndex2 = blocks.index(index, offsetBy: -1, limitedBy: blocks.count), blocks[minIndex2].isFree {
+            minIndex = minIndex2
+        }
+        
+        if let maxIndex2 = blocks.index(index, offsetBy: 1, limitedBy: blocks.count), blocks[maxIndex2].isFree {
+            maxIndex = maxIndex2
+        }
+        
+        let minBlock = blocks[minIndex]
+        let maxBlock = blocks[maxIndex]
+        
+        blocks.removeSubrange(minIndex...maxIndex)
+        let newBlock = Block(id: -1, range: minBlock.range.lowerBound..<maxBlock.range.upperBound, isFree: true)
+        blocks.insert(newBlock, at: minIndex)
     }
 }
 
-let manager = MemoryManager()
+let manager = MemoryManager(range: 100..<1250)
 
 manager.alloc(size: 250)
 manager.alloc(size: 100)
@@ -77,5 +117,5 @@ manager.free(id: 2)
 manager.alloc(size: 150)
 manager.alloc(size: 150)
 manager.alloc(size: 150)
-manager.free(id: 3)
-
+manager.free(id: 5)
+manager.alloc(size: 200)
